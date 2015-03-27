b887fadd659b6669f7feea1818d9a1a478126c18
s308 <s308@192.168.13.49>
2015-03-27 13:03:31 +0100

    added btn1

--- JavaAnfaenger/src/de/javaAnfang/SwingTest.java
+++ JavaAnfaenger/src/de/javaAnfang/SwingTest.java
@@ -1,27 +1,29 @@
 package de.javaAnfang;
 
-import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class SwingTest extends JFrame {
-
+	private static final long serialVersionUID = 6623929222893175152L;
+	
+	private JButton btn1;
+	
 	public SwingTest() throws HeadlessException {
-		this.setSize(200, 400);
+		this.setSize(300, 400);
+		this.setTitle("Swing JFrame Test");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
+		
+		btn1 = new JButton("Ok");
+		btn1.setSize(100,100);
+		btn1.setToolTipText("Die ist ein Tooltip auf dem Button " + btn1.getText());
+		
+		this.getContentPane().add(btn1);
 	}
 	
-	/**
-	 *
-	 * 
-	 */
-	private static final long serialVersionUID = 6623929222893175152L;
-
 	public static void main(String[] args) {
 		new SwingTest();
 	}
-
-
 }
